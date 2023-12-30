634f4de2fe9abdf5011debd0f8c002cb8ebbb006
ipatavatsizz <ankatryusuf@hotmail.com>
2023-12-30 19:18:08 +0300

    Updated

--- lib/package_annotations.dart
+++ lib/package_annotations.dart
@@ -1,8 +1,3 @@
 library package_annotations;
 
-import 'package:custom_lint_builder/custom_lint_builder.dart';
-import 'package:package_annotations/src/lint/package_lint.dart';
-
 export 'package:package_annotations/src/annotations/annotations.dart';
-
-PluginBase createPlugin() => PackageLinter();
